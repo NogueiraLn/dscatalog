00ede7f591d17060ed40d385638c3912650c6726
NogueiraLn <lucas.nogueiramartins@gmail.com>
2026-02-15 12:51:31 -0300

    Add: Custom validation responseValidationError

--- src/main/java/com/dscatalog/aula/resources/exceptions/StandardError.java
+++ src/main/java/com/dscatalog/aula/resources/exceptions/StandardError.java
@@ -9,15 +9,17 @@ public class StandardError implements Serializable {
 	private Instant timestamp;
 	private Integer status;
 	private String error;
+	private String message;
 	private String path;
 	
 	public StandardError() {
 	}
 
-	public StandardError(Instant timestamp, Integer status, String error, String path) {
+	public StandardError(Instant timestamp, Integer status, String error, String message, String path) {
 		this.timestamp = timestamp;
 		this.status = status;
 		this.error = error;
+		this.message = message;
 		this.path = path;
 	}
 
@@ -43,6 +45,14 @@ public class StandardError implements Serializable {
 
 	public void setError(String error) {
 		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
 
 	public String getPath() {
