4dffca32f6f942d070ada7f35a875064c3ed105c
NogueiraLn <lucas.nogueiramartins@gmail.com>
2026-02-23 00:05:19 -0300

    Add: Token ProductResource Tests

--- src/test/java/com/dscatalog/aula/resources/ProductResourceIT.java
+++ src/test/java/com/dscatalog/aula/resources/ProductResourceIT.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dscatalog.aula.dto.ProductDTO;
+import com.dscatalog.aula.tests.TokenUtil;
 import com.dscatalog.aula.tests.factories.ProductFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -28,16 +29,25 @@ public class ProductResourceIT {
 	private MockMvc mockMvc;
 	@Autowired
 	private ObjectMapper mapper;
+	@Autowired
+	private TokenUtil tokenUtil;
 	
 	private Long existingId;
 	private Long nonExistingId;
 	private Long countTotalProducts;
+	
+	private String username, password, bearerToken;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
 		nonExistingId = 1000L;
 		countTotalProducts = 25L;
+		
+		username = "maria@gmail.com";
+		password = "123456";
+		
+		bearerToken = tokenUtil.obtainAccessToken(mockMvc, username, password);
 		
 	}
 	
@@ -67,6 +77,7 @@ public class ProductResourceIT {
 		String jsonBody = mapper.writeValueAsString(productDTO);
 		
 		ResultActions result = mockMvc.perform(put("/products/{id}", existingId)
+				.header("Authorization", "Bearer " + bearerToken)
 				.content(jsonBody)
 				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON));
@@ -84,6 +95,7 @@ public class ProductResourceIT {
 		String jsonBody = mapper.writeValueAsString(productDTO);
 		
 		ResultActions result = mockMvc.perform(put("/products/{id}", nonExistingId)
+				.header("Authorization", "Bearer " + bearerToken)
 				.content(jsonBody)
 				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON));
